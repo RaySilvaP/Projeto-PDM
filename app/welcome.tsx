289406e88aa6ad1523fb82a60c13f4e5c49fdb3d
RaySilvaP <silvaray355@gmail.com>
2025-03-08 13:16:48 -0300

    Adicionando rederecionamento no primeiro launch

---
+++ app/welcome.tsx
@@ -0,0 +1,10 @@
+import { ThemedText } from '@/components/ThemedText';
+import {ThemedView} from '@/components/ThemedView';
+
+export default function Welcome() {
+    return (
+        <ThemedView>
+            <ThemedText>Test</ThemedText>
+        </ThemedView>
+    )
+}
